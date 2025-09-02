2892234c7f233f7e93005ab98f105736bacd78ea
JuanjoVillarejo <jota_villarejo22@hotmail.com>
2025-09-02 06:49:11 +0000

    resto de digitos

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
 function Home(props){
+    const seconds = props.seconds;
+
     return(<div className="bigCounter">
         <div className="reloj">⏰</div>
-        <div className="digito6">0</div>
-        <div className="digito5">0</div>
-        <div className="digito4">0</div>
-        <div className="digito3">0</div>
-        <div className="digito2">0</div>
-        <div className="digito1">{props.seconds % 10}</div>
+        <div className="digito6">{Math.floor(seconds / 100000) % 10}</div>
+        <div className="digito5">{Math.floor(seconds / 10000) % 10}</div>
+        <div className="digito4">{Math.floor(seconds / 1000) % 10}</div>
+        <div className="digito3">{Math.floor(seconds / 100) % 10}</div>
+        <div className="digito2">{Math.floor(seconds / 10) % 10}</div>
+        <div className="digito1">{seconds % 10}</div>
     </div>);
 }
 
-
-
 export default Home
+
+
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -22,6 +22,3 @@ setInterval(() => {
 )
   seconds ++;
 }, 1000);
-
-
-
